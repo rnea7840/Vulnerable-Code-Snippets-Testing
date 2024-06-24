4e16783b2dbefe22d34f25b3105c0ffcd4c55d97
rnea7840 <54969545+rnea7840@users.noreply.github.com>
2024-06-24 14:05:20 -0700

    Update cmd3.php

--- Command Injection/cmd3.php
+++ Command Injection/cmd3.php
@@ -1,4 +1,6 @@
-<?php     include("../common/header.php");   ?>
+<?php
+include("../common/header.php");
+?>
 
 <!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
 <?php
@@ -11,6 +13,10 @@ hint("will exec 'whois' with the arg specified in the GET parameter \"domain\"")
 
 <pre>
 <?php
-    system("/usr/bin/whois " . $_GET["domain"]);
+    // Validate and sanitize input
+    if (isset($_GET['domain'])) {
+        $domain = escapeshellarg($_GET['domain']);
+        system("/usr/bin/whois " . $domain);
+    }
  ?>
 </pre>
